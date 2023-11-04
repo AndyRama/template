9f074d7e069992e37d32ef45254f279c9e2a596a
Andy ramaroson <lemurianagency@gmail.com>
2023-11-04 16:20:37 +0100

    feat(hero) display main title , sub title and paragraph with animation

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 
 import Navigation from './components/Navigation'
-import Hero from './components/Hero'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +15,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Navigation/>
-        <Hero/>
         {children}
       </body>
     </html>
--- app/page.js
+++ app/page.js
@@ -1,7 +1,10 @@
+import Hero from "./components/Hero"
+import Image from "next/image"
 export default function Home() {
   return (
-    <main>
-      <h1>Home page</h1>
-    </main>
+    <>
+      {/* <h1>Home page</h1> */}
+      <Hero className="pt-32 pb-0 lg-pb:10"/>
+    </>
   )
 }
