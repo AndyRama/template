7e464bc2fbf9faea38f368979d7f5163032d40aa
Andy ramaroson <lemurianagency@gmail.com>
2023-11-05 23:46:32 +0100

    feat(about) finish component [missing test: not work]

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero"
+import About from "./components/About"
 import Image from "next/image"
 export default function Home() {
   return (
     <>
-      <Hero className="pt-32 pb-0 lg-pb:10" data-testid="hero" />
+      <Hero className="pt-32 pb-0 lg-pb:10" />
+      <About className="py-16 lg:py-32 overflow-hidden"/>
     </>
   )
 }
