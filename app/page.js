6324e036ad1e44adfedff7e8399791ca6ee4ed8f
Andy ramaroson <lemurianagency@gmail.com>
2023-11-06 16:37:20 +0100

    feat(howIwork) finish component [missing test: not work]

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero"
 import About from "./components/About"
+import HowIWork from "./components/HowIWork"
 import Image from "next/image"
 export default function Home() {
   return (
     <>
       <Hero className="pt-32 pb-0 lg-pb:10" />
       <About className="py-16 lg:py-32 overflow-hidden"/>
+      <HowIWork className="py-16 !pt-32 lg:py-32"/>
     </>
   )
 }
