3e5e06a493ec66736e59206be3e1163a866bca3c
Andy ramaroson <lemurianagency@gmail.com>
2023-11-08 21:40:16 +0100

    feat(RecentBlog) add component RecentBlog

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import Hero from "./components/Hero"
 import About from "./components/About"
 import HowIWork from "./components/HowIWork"
 import Testimonial from "./components/Testimonial"
+import RecentBlog from "./components/RecentBlog"
 import Image from "next/image"
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <About className="py-16 lg:py-32 overflow-hidden"/>
       <HowIWork className="py-16 !pt-32 lg:py-32"/>
       <Testimonial className="py-6 lg:py-32"/>
+      <RecentBlog className="pt-14 pb-15 lg:pb-32"/>
     </>
   )
 }
